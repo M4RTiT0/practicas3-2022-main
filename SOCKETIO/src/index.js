35e16835ddcda35d40dd2bb35cc4215b63ef184d
M4RTiT0 <martotamborini8@gmail.com>
2022-11-15 15:20:39 -0300

    ghjk,.

--- SOCKETIO/src/index.js
+++ SOCKETIO/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const socketio = require('socket.io') 
 
 const server = express()
 
@@ -9,4 +10,12 @@ server.use(cors())
 
 server.listen(server.get('port'), () => {
     console.log('la conexion fue exitosa!')
+});
+
+//le pasamos el servidor a socketio para habilitar el canal websocket
+const io = socketio(servidor)
+
+//establecemos la apertura del canal para emitir posteriormente los datos
+io.on('connection', (socket) => {
+    socket.emit('dato-socket', 10)
 });
