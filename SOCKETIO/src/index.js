0b111e9082d0c24aff05992616d38b79e1dcad70
M4RTiT0 <martotamborini8@gmail.com>
2022-11-15 14:43:53 -0300

    fghjgh

--- SOCKETIO/src/index.js
+++ SOCKETIO/src/index.js
@@ -2,3 +2,11 @@ const express = require('express')
 const cors = require('cors')
 
 const server = express()
+
+server.set('port', process.env.PORT || 3000)
+
+server.use(cors())
+
+server.listen(server.get('port'), () => {
+    console.log('la conexion fue exitosa!')
+});
